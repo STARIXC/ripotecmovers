40530c0c644759c305d7730442d607cf3fb5a171
BWAHYI CHRIS MUTHEE <30801550+STARIXC@users.noreply.github.com>
2024-02-12 22:05:34 +0300

    Add files via upload

---
+++ storage/framework/views/5221106390a190ab8f2e9bcacaa534a5.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Count - Static</title>
+    <meta name="description" content="">
+    <meta name="author" content="Starixc Tech Solutions">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="<?php echo e(asset('css/base.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('css/vendor.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('css/main.css')); ?>">
+
+    <!-- script
+    ================================================== -->
+    <script src="<?php echo e(asset('js/modernizr.js')); ?>"></script>
+    <script src="<?php echo e(asset('js/pace.min.js')); ?>"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+
+    <!-- home
+    ================================================== -->
+    <main class="s-home s-home--static">
+
+        <div class="overlay"></div>
+
+        <div class="home-content">
+
+            <div class="home-logo">
+                <a href="<?php echo e('/'); ?>">
+                    
+                    Rivotec Movers and Homes
+                </a>
+            </div>
+
+            <div class="row home-content__main">
+
+                <div class="col-eight home-content__text pull-right">
+                    <h3>Welcome to Rivotec Movers and Homes!</h3>
+
+                    <h1>
+                        We are currently working <br>
+                        on a new super awesome <br> website.
+                    </h1>
+
+                    <p>
+                        At Rivotec, we are dedicated to providing exceptional services to make your stay and travels
+                        seamless and comfortable. Whether you're looking for temporary accommodation through our BNB
+                        services, reliable transportation options, or convenient airport pick-ups and drop-offs, we've
+                        got you covered.
+                    </p>
+
+                    
+                </div> <!-- end home-content__text -->
+
+                <div class="col-four home-content__counter">
+                    <h3>Launching In</h3>
+
+                    <div class="home-content__clock">
+                        <div class="top">
+                            <div class="time days">
+                                30
+                                <span>Days</span>
+                            </div>
+                        </div>
+                        <div class="time hours">
+                            09
+                            <span>H</span>
+                        </div>
+                        <div class="time minutes">
+                            54
+                            <span>M</span>
+                        </div>
+                        <div class="time seconds">
+                            30
+                            <span>S</span>
+                        </div>
+                    </div> <!-- end home-content__clock -->
+                </div> <!-- end home-content__counter -->
+
+            </div> <!-- end home-content__main -->
+
+
+
+            <div class="row home-copyright">
+                <span>Copyright Count 2024</span>
+                <span>Design by <a href="https://www.starixc.co.ke/">Starixc Tech Solutions</a></span>
+            </div> <!-- end home-copyright -->
+
+            <div class="home-content__line"></div>
+
+        </div> <!-- end home-content -->
+
+    </main> <!-- end s-home -->
+
+    <!-- info
+    ================================================== -->
+    <a class="info-toggle" href="#0">
+        <span class="info-menu-icon"></span>
+    </a>
+
+    <div class="s-info">
+
+        <div class="row info-wrapper">
+
+            <div class="col-seven tab-full info-main">
+                <h1>We are Rivotec Movers and Homes!.</h1>
+                <p>
+                    we are dedicated to providing exceptional services to make your stay and travels seamless and
+                    comfortable. Whether you're looking for temporary accommodation through our BNB services, reliable
+                    transportation options, or convenient airport pick-ups and drop-offs, we've got you covered.
+                </p>
+
+                <p>
+                    Our team is committed to ensuring that your experience with us exceeds your expectations. From the
+                    moment you arrive until your departure, we strive to provide top-notch hospitality and assistance
+                    every step of the way.
+                </p>
+                <p>Experience the convenience and reliability of Rivotec Movers and Homes. Let us take care of the
+                    details while you focus on enjoying your journey.</p>
+            </div>
+
+            <div class="col-four tab-full pull-right info-contact">
+
+                <div class="info-block">
+                    <h3>Start A Conversation</h3>
+                    <p>
+                        <a href="mailto:#0" class="info-email">info@rivotecmovers.co.ke</a><br>
+                        <a href="tel:+254722790754" class="'info-phone">+254.722.790.754</a>
+                    </p>
+                </div>
+
+                <div class="info-block">
+                    <h3>Visit Our Office</h3>
+
+                    <p class="info-address">
+                        Nanyuki<br>
+                        
+                    </p>
+                </div>
+
+                <div class="info-block">
+                    <h3>Find Us On</h3>
+
+                    <ul class="info-social">
+                        <li>
+                            <a href="#0"><i class="fab fa-facebook" aria-hidden="true"></i>
+                                <span>Facebook</span></a>
+                        </li>
+                        <li>
+                            <a href="#0"><i class="fab fa-twitter" aria-hidden="true"></i>
+                                <span>Twiiter</span></a>
+                        </li>
+                        <li>
+                            <a href="#0"><i class="fab fa-instagram" aria-hidden="true"></i>
+                                <span>Instagram</span></a>
+                        </li>
+                        
+                    </ul>
+                </div>
+
+            </div> <!-- end info contact -->
+
+        </div> <!-- end info wrapper -->
+
+    </div> <!-- end s-info -->
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale-pulse-out">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="<?php echo e(asset('js/jquery-3.2.1.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('js/plugins.js')); ?>"></script>
+    <script src="<?php echo e(asset('js/main.js')); ?>"></script>
+    
+</body>
+
+</html>
+<?php /**PATH C:\Users\UTJ\Pictures\website\ripotecmovers\resources\views/coming-soon/index.blade.php ENDPATH**/ ?>
